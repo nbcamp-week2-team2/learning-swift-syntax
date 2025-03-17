3781e1b5c499c1c262797701050dda5ce2be257c
EllGHT <rmsgh1188@gmail.com>
2025-03-17 19:46:41 +0900

    feat: 필수문제 2 구현

--- geunho/LearningSwiftSyntax/LearningSwiftSyntax/main.swift
+++ geunho/LearningSwiftSyntax/LearningSwiftSyntax/main.swift
@@ -27,3 +27,45 @@ func calculate(_ sum: (Int, Int) -> String) {
 }
 
 calculate(sum)
+
+
+// 필수 문제 2
+let numbers = [1, 2, 3, 4, 5]
+
+//아래 forEach 문을 map 을 사용하는 코드로 변환해주세요.
+//var result = [String]()
+//
+//for number in numbers {
+//    result.append(number)
+//}
+
+var result: [String] = numbers.map { String($0) }
+print(result)
+
+
+// 주어진 입력값을 고차함수를 체이닝하여 주어진 출력값이 나오도록 구현해주세요.
+let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let output = input.filter { $0 % 2 == 0 }.map { String($0) }
+print(output)
+
+
+//이제 고차함수를 직접 만들어 봅니다.
+//함수명: myMap
+//파라미터는 2개 입니다.
+//배열: [Int]
+//변환 클로저: (Int) → String
+//반환 값은 1개 입니다.
+//[String]
+func myMap(_ arr: [Int], _ closure: (Int) -> String) -> String {
+    var mapped = String()
+    for elem in arr {
+        mapped.append(closure(elem))
+    }
+    return mapped
+}
+
+let myMapResult = myMap([1, 2, 3, 4, 5]) {
+    String($0)
+}
+
+print("myMap: \(result)") // ["1", "2", "3", "4", "5"]
