419577e7f6b2d87a4d48724904f409e1281e1b20
imo2k <dnrwk99@gmail.com>
2025-03-18 14:02:25 +0900

    feat: 필수 문제 1번 구현 완료

--- ukhyun/swift-syntax/swift-syntax/main.swift
+++ ukhyun/swift-syntax/swift-syntax/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
+/*
+ 두 개의 Int 값을 파라미터로 받고, 하나의 String 값을 반환하는 클로저
+ */
 
+// MARK: - 필수 1번
+
+// 클로저 저장할 상수 선언
+let sum: (Int, Int) -> String = { num1, num2 in
+    let result = num1 + num2
+    return "두 수의 합은 \(result)입니다"
+}
+print(sum(3, 6))
+
+// 클로저를 파라미터로 받는 함수
+func calculate(_ closure: (Int, Int) -> String) {
+    let result = closure(5, 6)
+    print(result)
+}
+
+calculate(sum)
