6fb61c54495cdc95db535e099d837169cef625d1
imo2k <dnrwk99@gmail.com>
2025-03-18 17:10:18 +0900

    feat: 필수 문제 2번 구현 완료

--- ukhyun/swift-syntax/swift-syntax/main.swift
+++ ukhyun/swift-syntax/swift-syntax/main.swift
@@ -7,11 +7,9 @@
 
 import Foundation
 
-/*
- 두 개의 Int 값을 파라미터로 받고, 하나의 String 값을 반환하는 클로저
- */
-
 // MARK: - 필수 1번
+
+// 두 개의 Int 값을 파라미터로 받고, 하나의 String 값을 반환하는 클로저
 
 // 클로저 저장할 상수 선언
 let sum: (Int, Int) -> String = { num1, num2 in
@@ -27,3 +25,30 @@ func calculate(_ closure: (Int, Int) -> String) {
 }
 
 calculate(sum)
+
+// MARK: - 필수 2번
+
+// map 을 사용하는 코드로 변환
+let numbers = [1, 2, 3, 4, 5]
+let result = numbers.map { String($0) }
+print(result)
+
+// 주어진 입력값을 고차함수를 체이닝하여 주어진 출력값이 나오도록 구현
+let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let output = input.filter { $0 % 2 == 0 }.map { String($0) }
+
+print(output)
+
+// myMap
+func myMap(_ array: [Int], conversion: (Int) -> String) -> [String] {
+    var result = [String]()
+    
+    for i in array {
+        result.append(conversion(i))
+    }
+    
+    return result
+}
+
+let customMap = myMap([1,2,3,4,5]) { String($0) }
+print(customMap)
