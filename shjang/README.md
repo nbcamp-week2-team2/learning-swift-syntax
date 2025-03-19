9110963f6eaea54b4f1f2f03907a09f10062ac64
Seungho Jang <sjang1594@gmail.com>
2025-03-19 11:06:12 +0900

    feat: Rather using switch, use as struct to take an separate commands

--- shjang/README.md
+++ shjang/README.md
@@ -1,5 +1,15 @@
 ## This Project refers to : [Project Description](https://teamsparta.notion.site/Ch-2-1b82dc3ef51480708c52c22b28f47d4b)
 
+### Directory Tree
+```bash
+├── Package.resolved
+├── Package.swift
+├── README.md
+└── Sources
+    ├── Class.swift
+    └── LearningSwiftCli.swift
+```
+
 ### The Project Setup
 * In order to build a command line tool, it's better to follow the swift documentation instead of opening the XCode and select `Cli`. Using Swift Package Manager(SwiftPM), you can manage or import other libraries into your cli tools
 
@@ -11,9 +21,18 @@
 * If you really want to test this program in XCode, which is unnecessary! You can follow the direction. 
 `LearningSwiftCli` -> `Edit Scheme` -> `Arguments` -> Add all arguments you want to passed on launch (ex: `--input 1`).
 
-### List of Command Options
-* commands are following fashion. 
+### List of Command Line Args
+* commands are following fashion: 
+```bash
+swift run LearningSwiftCli closure 1 2
+swift run LearningSwiftCli map 1 2 3 4 5
+swift run LearningSwiftCli arr 1 2 3 4 5 6 7 
+swift run LearningSwiftCli arr A B C D E
+swift run LearningSwiftCli class
+```
 
+### !One Possible Downside!
+* when commands are paresed, I haven't figured out how to handle generic type for incoming argument. One possible way is to set generic `ArrayComprehension`, then take the `args` as `[T]`. But the essential implementations are on `func arrayComprehension(arr:)` 
 
 ### Dependency: Arugment Parser
 * Helper to takes the arguments from command line.
--- shjang/Sources/LearningSwiftCli.swift
+++ shjang/Sources/LearningSwiftCli.swift
@@ -2,106 +2,148 @@ import ArgumentParser
 
 @main
 struct LearningSwiftCli: ParsableCommand {
-    @Option(help: "Select Practice exercise to run (1-4) ")
-    var input: Int
-    
-    init() {}
-    
-    mutating func run() throws {
-        switch input {
-        case 1:
-            closure()
-            break;
-        case 2:
-            usageOfMap()
-            break;
-        case 3:
-            arrayComprehension()
-            break;
-        case 4:
-            classImpl()
-            break;
-        default:
-            print("Invalid Input")
-            throw ExitCode.failure
+    static let configuration = CommandConfiguration(
+        abstract: "Swift Learning",
+        subcommands: [Closure.self, MapUsage.self, ArrayComprehension.self,
+                      ClassImpl.self]
+    )
+}
+
+extension LearningSwiftCli {
+    struct Closure: ParsableCommand {
+        @Argument(help: "Closure - Args must be Int")
+        var args: [Int]
+        static let configuration = CommandConfiguration(commandName: "closure")
+        func run() throws {
+            closure(arr: args)
         }
-    }
-    
-    public func closure() {
-        let sum:(Int, Int) -> String = { return String("Total Sum: \($0) + \($1)") }
-    
-        let result = sum(5, 6)
-        print("Result of Closure  : \(result)\n")
-        
-        print("Function Signature : calculate (_ op: (Int, Int) -> String ) ")
-        func calculate(_ op: (Int, Int) -> String) {
-            print("Result of calculate: \(op(8, 9))\n")
-        }
-        
-        calculate(sum)
-    }
-    
-    public func usageOfMap() {
-        let numbers = [1, 2, 3, 4, 5]
-        let result = numbers.map { String($0) }
-        print("Result Array    : \(result)")
-        
-        // TODO: Take this as second arguments
-        let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        print("Input Array is  : \(input)")
-        let output = input.filter { $0 % 2 == 0 }.map { String($0) }
-        print("Output Array is : \(output)\n")
-        
-        // Custom High-Order Function
-        func myMap(_ arr: [Int], _ transform: (Int) -> String) -> [String] {
-            var res: [String] = []
-            for elm in arr {
-                res.append(transform(elm))
-            }
-            return res
-        }
-        
-        let resultHighOrder = myMap(numbers) { String($0) }
-        print("Results of High Order Func : \(resultHighOrder)")
-    }
-    
-    public func arrayComprehension() {
-        print("Function Signature : a(_ arr:[Int]) -> [Int]")
-        let arr = [1,2,3,4,5]
-        let strArr = ["A", "B", "C", "D", "E"]
-        // https://www.reddit.com/r/swift/comments/4hjgkk/swift_string_access_odd_or_even_characters/
-        func a(_ arr:[Int]) -> [Int] {
-            return arr.enumerated().filter { $0.0 % 2 == 0}.map { $0.1 }
-        }
-        print("\(a(arr))\n")
-        
-        print("Function Signautre: b(_ arr:[String]) -> [String]")
-        func b(_ arr: [String]) -> [String] {
-            return arr.enumerated().filter { $0.0 % 2 == 0}.map { $0.1 }
-        }
-        print("\(b(strArr))\n")
-        
-        // Generic
-        print("Function Signature: c<T> (_ arr:[T]) -> [T]")
-        func c<T>(_ arr: [T]) -> [T] {
-            return arr.enumerated().filter { $0.0 % 2 == 0}.map { $0.1 }
-        }
-        print("\(c(strArr))")
-        print("\(c(arr))\n")
-        
-        print("Function Signature: d<T: Numeric> (_ arr:[T]) -> [T]")
-        func d<T: Numeric>(_ arr: [T]) -> [T] {
-            return arr.enumerated().filter { $0.0 % 2 == 0}.map { $0.1 }
-        }
-    }
-    
-    public func classImpl () {
-        // Protocol 채택하면, 코드의 길어지지만, 기능의 확장성은 넓어져갑니다. 그 의미는 어느 정도의 상속의 특징을 물려받지만,
-        //                  하지만 계층 구조를 파악하기 어려울 가능성도 있음
-        // Inheritance: 다중상속 가능 -> 모든 Child 가 다 물려받고, 심지어 쓸데 없는 메모리를 잡아먹을수 있음.
-        // 어찌됬든 목적에 맞게 적절하게 사용
-        let hybridCar = HybridCar(brand: "Nissan", model: "Altima", yearMade: "2021")
-        let electricalEngine = ElectricEngine(type: "Electric")
-        hybridCar.switchEngine(to: electricalEngine)
     }
 }
+
+extension LearningSwiftCli {
+    struct MapUsage: ParsableCommand {
+        static let configuration = CommandConfiguration(commandName: "map")
+        @Argument(help: "Map - Args must be Int")
+        var args: [Int]
+        func run() throws {
+            usageOfMap(arr: args)
+        }
+    }
+}
+
+extension LearningSwiftCli {
+    struct ArrayComprehension: ParsableCommand {
+        static let configuration = CommandConfiguration(commandName: "arr")
+        @Argument(help: "Arr - Args must be String or Int")
+        var args: [String] // Default
+        func run() throws {
+            if args.allSatisfy({ Int($0) != nil }) {
+                let intArr = args.map { Int($0) }
+                arrayComprehension(arr: intArr)
+            } else {
+                arrayComprehension(arr: args)
+            }
+        }
+    }
+}
+
+extension LearningSwiftCli {
+    struct ClassImpl: ParsableCommand {
+        static let configuration = CommandConfiguration(commandName: "class")
+        func run() throws {
+            classImpl()
+        }
+    }
+}
+
+func closure(arr: [Int]) {
+    let sum:(Int, Int) -> String = { return String("Total Sum: \($0) + \($1)") }
+    let result = sum(5, 6)
+    print("Result of Closure  : \(result)\n")
+        
+    print("Function Signature : calculate (_ op: (Int, Int) -> String ) ")
+    func calculate(_ op: (Int, Int) -> String) {
+        print("Result of calculate: \(op(8, 9))\n")
+    }
+    calculate(sum)
+}
+    
+func usageOfMap(arr: [Int]) {
+    let result = arr.map { String($0) }
+    print("Result Array    : \(result)")
+    
+    print("Input Array is  : \(arr)")
+    let output = arr.filter { $0 % 2 == 0 }.map { String($0) }
+    print("Output Array is : \(output)\n")
+    
+    // Custom High-Order Function
+    func myMap(_ arr: [Int], _ transform: (Int) -> String) -> [String] {
+        var res: [String] = []
+        for elm in arr {
+            res.append(transform(elm))
+        }
+        return res
+    }
+    
+    let resultHighOrder = myMap(arr) { String($0) }
+    print("Results of High Order Func : \(resultHighOrder)")
+}
+    
+func arrayComprehension<T>(arr: [T]) {
+    /// https://www.reddit.com/r/swift/comments/4hjgkk/swift_string_access_odd_or_even_characters/
+    func a(_ arr:[Int]) -> [Int] {
+        return arr.enumerated().filter { $0.0 % 2 == 0}.map { $0.1 }
+    }
+    
+    func b(_ arr: [String]) -> [String] {
+        return arr.enumerated().filter { $0.0 % 2 == 0}.map { $0.1 }
+    }
+    
+    func c<Y>(_ arr: [Y]) -> [Y] {
+        return arr.enumerated().filter { $0.0 % 2 == 0}.map { $0.1 }
+    }
+    
+    func d<C: Numeric>(_ arr: [C]) -> [C] {
+        return arr.enumerated().filter { $0.0 % 2 == 0}.map { $0.1 }
+    }
+    
+    if let intArr = arr as? [Int] {
+        print("Function Signature : a(_ arr:[Int]) -> [Int]")
+        print("\(a(intArr))\n")
+    } else if let strArr = arr as? [String] {
+        print("Function Signature : b(_ arr:[String]) -> [String]")
+        print("\(b(strArr))\n")
+    } else {
+        // Generic
+        print("Function Signature: c<Y> (_ arr:[Y]) -> [Y]")
+        print("\(c(arr))\n")
+    
+        print("Function Signature: d<C: Numeric> (_ arr:[C]) -> [C]")
+        if let numericArr = arr as? [any Numeric] {
+            let convertArr = numericArr.compactMap { value -> Double? in
+                if let doubleValue = value as? Double {
+                    return doubleValue
+                } else if let intValue = value as? Int {
+                    return Double(intValue)
+                } else if let floatValue = value as? Float {
+                    return Double(floatValue)
+                } else {
+                    print("Failed to Convert to Any Value in Numeric")
+                    return nil
+                }
+            }
+            print("\(d(convertArr))")
+        }
+    }
+}
+    
+func classImpl() {
+    // Protocol 채택하면, 코드의 길어지지만, 기능의 확장성은 넓어져갑니다. 그 의미는 어느 정도의 상속의 특징을 물려받지만,
+    //                  하지만 계층 구조를 파악하기 어려울 가능성도 있음
+    // Inheritance: 다중상속 가능 -> 모든 Child 가 다 물려받고, 심지어 쓸데 없는 메모리를 잡아먹을수 있음.
+    // 어찌됬든 목적에 맞게 적절하게 사용
+    let hybridCar = HybridCar(brand: "Nissan", model: "Altima", yearMade: "2021")
+    let electricalEngine = ElectricEngine(type: "Electric")
+    hybridCar.switchEngine(to: electricalEngine)
+}
+
