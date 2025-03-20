8a9dc7b0128f81195bf1a15f7da6b3594b1dc32c
youseokhwan <youseokhwan15@gmail.com>
2025-03-20 12:35:13 +0900

    fix: myMap을 주어진 요구사항에 맞게 함수 형태로 변경

--- seokhwan/LearningSwiftSyntax/Sources/EssentialTasks/Task2.swift
+++ seokhwan/LearningSwiftSyntax/Sources/EssentialTasks/Task2.swift
@@ -1,6 +1,6 @@
 /*
  [Int], (Int) -> String를 파라미터로 받고 [String]을 반환하는 myMap 고차함수 작성
  */
-let myMap: ([Int], (Int) -> String) -> [String] = { numbers, closure in
+func myMap(_ numbers: [Int], _ closure: (Int) -> String) -> [String] {
     return numbers.map { closure($0) }
 }
